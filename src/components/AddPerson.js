6273d81db11f1674687635b6c30d82e4174c4fe7
Dikla <dikla.aharoni@gmail.com>
2018-07-14 00:31:41 -0700

    Css

--- src/components/AddPerson.js
+++ src/components/AddPerson.js
@@ -66,97 +66,98 @@ class AddPerson extends Component {
   render() {
     return (
       <ScrollView showsVerticalScrollIndicator={false}>
-      <View style={styles.form}>
-      <Text style={styles.text}>ADD A NEW CONTACT</Text>
-      <View>
-      <Image sorce={{uri: this.state.avatar}}
-      style={styles.avatar}
-      />
-      <AddImage
-      onPress={()=>this.addImage()}
-      />
-      </View>
-      <MKTextField
-      textInputStyle={styles.fieldStyles}
-      placeholder={'First name...'}
-      tintColor={MKColor.Blue}
-      value={this.props.first_name}
-      onChangeText={value => this.props.formUpdate({ prop: 'first_name', value})}
-      />
-      <MKTextField
-      textInputStyle={styles.fieldStyles}
-      placeholder={'Last name...'}
-      tintColor={MKColor.Blue}
-      value={this.props.last_name}
-      onChangeText={value => this.props.formUpdate({ prop: 'last_name', value})}
-      />
-      <MKTextField
-      textInputStyle={styles.fieldStyles}
-      placeholder={'Phone number...'}
-      tintColor={MKColor.Blue}
-      value={this.props.phone}
-      onChangeText={value => this.props.formUpdate({ prop: 'phone', value})}
-      />
-      <MKTextField
-      textInputStyle={styles.fieldStyles}
-      placeholder={'Email...'}
-      tintColor={MKColor.Blue}
-      value={this.props.email}
-      onChangeText={value => this.props.formUpdate({ prop: 'email', value})}
-      />
-      <MKTextField
-      textInputStyle={styles.fieldStyles}
-      placeholder={'Company...'}
-      tintColor={MKColor.Blue}
-      value={this.props.company}
-      onChangeText={value => this.props.formUpdate({ prop: 'company', value})}
-      />
-      <MKTextField
-      textInputStyle={styles.fieldStyles}
-      placeholder={'Instagram...'}
-      tintColor={MKColor.Blue}
-      value={this.props.instagram}
-      onChangeText={value => this.props.formUpdate({ prop: 'instagram', value})}
-      />
-      <MKTextField
-      textInputStyle={styles.fieldStyles}
-      placeholder={'LinkedIn...'}
-      tintColor={MKColor.Blue}
-      value={this.props.linkedin}
-      onChangeText={value => this.props.formUpdate({ prop: 'linkedin', value})}
-      />
-      <MKTextField
-      textInputStyle={styles.fieldStyles}
-      placeholder={'Facebook...'}
-      tintColor={MKColor.Blue}
-      value={this.props.facebook}
-      onChangeText={value => this.props.formUpdate({ prop: 'facebook', value})}
-      />
-      <MKTextField
-      textInputStyle={styles.fieldStyles}
-      placeholder={'Twitter...'}
-      tintColor={MKColor.Blue}
-      value={this.props.twitter}
-      onChangeText={value => this.props.formUpdate({ prop: 'twitter', value})}
-      />
-      <MKTextField
-      textInputStyle={styles.fieldStyles}
-      placeholder={'Project...'}
-      tintColor={MKColor.Blue}
-      value={this.props.project}
-      onChangeText={value => this.props.formUpdate({ prop: 'project', value})}
-      />
-      <MKTextField
-      textInputStyle={styles.fieldStyles}
-      placeholder={'Notes...'}
-      tintColor={MKColor.Blue}
-      value={this.props.notes}
-      onChangeText={value => this.props.formUpdate({ prop: 'notes', value})}
-      />
-      <View style={styles.addButton}>
-      <AddButton onPress={this.onAddPress.bind(this)}/>
-      </View>
-      </View>
+        <View style={styles.form}>
+          <Text style={styles.text}>ADD A NEW CONTACT</Text>
+        <View>
+          <Image
+            sorce={{uri: this.state.avatar}}
+            style={styles.avatar}
+          />
+          <AddImage
+            onPress={()=>this.addImage()}
+          />
+        </View>
+          <MKTextField
+          textInputStyle={styles.fieldStyles}
+          placeholder={'First name...'}
+          tintColor={MKColor.Blue}
+          value={this.props.first_name}
+          onChangeText={value => this.props.formUpdate({ prop: 'first_name', value})}
+          />
+          <MKTextField
+          textInputStyle={styles.fieldStyles}
+          placeholder={'Last name...'}
+          tintColor={MKColor.Blue}
+          value={this.props.last_name}
+          onChangeText={value => this.props.formUpdate({ prop: 'last_name', value})}
+          />
+          <MKTextField
+          textInputStyle={styles.fieldStyles}
+          placeholder={'Phone number...'}
+          tintColor={MKColor.Blue}
+          value={this.props.phone}
+          onChangeText={value => this.props.formUpdate({ prop: 'phone', value})}
+          />
+          <MKTextField
+          textInputStyle={styles.fieldStyles}
+          placeholder={'Email...'}
+          tintColor={MKColor.Blue}
+          value={this.props.email}
+          onChangeText={value => this.props.formUpdate({ prop: 'email', value})}
+          />
+          <MKTextField
+          textInputStyle={styles.fieldStyles}
+          placeholder={'Company...'}
+          tintColor={MKColor.Blue}
+          value={this.props.company}
+          onChangeText={value => this.props.formUpdate({ prop: 'company', value})}
+          />
+          <MKTextField
+          textInputStyle={styles.fieldStyles}
+          placeholder={'Instagram...'}
+          tintColor={MKColor.Blue}
+          value={this.props.instagram}
+          onChangeText={value => this.props.formUpdate({ prop: 'instagram', value})}
+          />
+          <MKTextField
+          textInputStyle={styles.fieldStyles}
+          placeholder={'LinkedIn...'}
+          tintColor={MKColor.Blue}
+          value={this.props.linkedin}
+          onChangeText={value => this.props.formUpdate({ prop: 'linkedin', value})}
+          />
+          <MKTextField
+          textInputStyle={styles.fieldStyles}
+          placeholder={'Facebook...'}
+          tintColor={MKColor.Blue}
+          value={this.props.facebook}
+          onChangeText={value => this.props.formUpdate({ prop: 'facebook', value})}
+          />
+          <MKTextField
+          textInputStyle={styles.fieldStyles}
+          placeholder={'Twitter...'}
+          tintColor={MKColor.Blue}
+          value={this.props.twitter}
+          onChangeText={value => this.props.formUpdate({ prop: 'twitter', value})}
+          />
+          <MKTextField
+          textInputStyle={styles.fieldStyles}
+          placeholder={'Project...'}
+          tintColor={MKColor.Blue}
+          value={this.props.project}
+          onChangeText={value => this.props.formUpdate({ prop: 'project', value})}
+          />
+          <MKTextField
+          textInputStyle={styles.fieldStyles}
+          placeholder={'Notes...'}
+          tintColor={MKColor.Blue}
+          value={this.props.notes}
+          onChangeText={value => this.props.formUpdate({ prop: 'notes', value})}
+          />
+          <View style={styles.addButton}>
+          <AddButton onPress={this.onAddPress.bind(this)}/>
+          </View>
+        </View>
       </ScrollView>
     );
   }
